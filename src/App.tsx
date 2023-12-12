6acaf5e8c1d6d5b7018a1d5219f888a7fffd186a
vadymrass <>
2023-12-12 22:24:26 +0000

    changed router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import Router from './router'
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Router />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
 

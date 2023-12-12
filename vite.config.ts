4c0a0b287a60f571d6abaadde11839fca8f49e7c
vadymrass <>
2023-12-12 22:40:07 +0000

    added the vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist" // tells vite to output the build to a folder called docs
-  },
-  base: '/'
 })

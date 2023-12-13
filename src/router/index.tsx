a0b289bbfdc2053a182da9e99a78a6be8b1f2fa5
vadymrass <>
2023-12-13 09:16:50 +0000

    added default roter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes, Navigate } from 'react-router-dom'
 import SignIn from '../pages/auth/signin'
 import ResetPassword from '../pages/auth/reset-password'
 import NewPassword from '../pages/auth/new-password'
@@ -7,6 +7,7 @@ import OverView from '../pages/overview'
 const Router = () => {
     return (
         <Routes>
+            <Route path='/' element={<Navigate to={'/overview'} />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/new-password" element={<NewPassword />} />
